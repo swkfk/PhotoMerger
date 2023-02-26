694e743ced4a11abf5f3a38ea356fe16387b8d84
kai_Ker <kai_Ker@buaa.edu.cn>
2023-02-26 10:34:02 +0800

    增加对 termcolor 缺失的特殊处理

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 import os
+import sys
+from typing import Any, Iterable
+
+try:
+    from termcolor import cprint
+except ImportError:
+    print("[W]无法导入彩色模块，可以使用 `pip install termcolor` 进行安装", file=sys.stderr)
+
+
+    def cprint(
+            text: str,
+            color: str | None = None,
+            on_color: str | None = None,
+            attrs: Iterable[str] | None = None,
+            **kwargs: Any,
+    ) -> None:
+        print(text, **kwargs)
 
 try:
     from PIL import Image
 except ImportError:
-    print("[C]无法导入图片处理模块，请使用 `pip install Pillow` 进行安装")
-    os.system("pause")
+    cprint("[C]无法导入图片处理模块，请使用 `pip install Pillow` 进行安装",
+           color="red", attrs=["bold"], file=sys.stderr)
+    os.system("pause>nul")
+    sys.exit(1)
